11b51630f39faf6fce7a00f918f67427815c614d
Shamik <shamik45@gmail.com>
2017-11-17 23:11:58 -0800

    small change to sql bootstrap file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO SA.PHOTO_INFO(NAME, ALBUM_ID) VALUES ('2hamik', '1000000414802924' );
-INSERT INTO SA.PHOTO_INFO(NAME, ALBUM_ID, PEOPLE_IN_PHOTOS) VALUES ('Shamik', '1000000426920616', 'Anshi, Foram, Shamik, Preesha, Shrvya, Sweta, Geeta Yagnik, Nayan Yagnik, Khushboo Modi, Priyanki Shah, Jatin Bhana, Kaushal, Neeyam, Deepak, Agastya, Venkatesh, pradip shah' );
+INSERT INTO SA.PHOTO_INFO(NAME, ALBUM_ID, PEOPLE_IN_PHOTOS) VALUES ('Shamik', '1000000426920616', 'Anshi, Foram, Shamik, Preesha, Shrvya, Sweta, Geeta Yagnik, Nayan Yagnik, Khushboo Modi, Priyanki Shah, Jatin Bhana, Kaushal, Neeyam, Deepak, Agastya, pradip shah' );
 
 INSERT INTO SA.CALENDAR_INFO(NAME, CALENDAR_URL) VALUES ('2hamik', 'https://www.googleapis.com/calendar/v3/calendars/shamik.nfc@gmail.com/events?orderBy=startTime&singleEvents=true');
 INSERT INTO SA.CALENDAR_INFO(NAME, CALENDAR_URL) VALUES ('Shamik', 'https://www.googleapis.com/calendar/v3/calendars/0h04lenjqb302dao7lhujenmkk@group.calendar.google.com/events?orderBy=startTime&singleEvents=true');
